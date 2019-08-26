77c20c294894b3029259ffd757ea362a61689abf
torikraju <torikraju@gmail.com>
2019-08-26 18:50:10 +0600

    delete client related file

--- client/src/store/actions/index.js
+++
@@ -1,1 +0,0 @@
-// export { tryAuth, registration } from './auth';
--- client/src/utility/AppUtil.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const updateObject = (oldObject, updatedProperties) => {
-  return {
-    ...oldObject,
-    ...updatedProperties,
-  };
-};
